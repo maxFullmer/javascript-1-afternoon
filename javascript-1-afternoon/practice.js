fcec6697d839c3ee2b2b7c58633c28ce8176a7bb
Max-F <max.fullmer@outlook.com>
2019-06-11 13:55:12 -0700

    first save

--- javascript-1-afternoon/practice.js
+++ javascript-1-afternoon/practice.js
@@ -2,25 +2,26 @@
 
 //Create a variable called myName that is a string data type
 
-//Code here
+let myName = "Michael";
 
 //////////////////PROBLEM 2////////////////////
 
 //Create a variable called myAge that is a number data type
 
-//Code here
+let myAge = 55;
 
 //////////////////PROBLEM 3////////////////////
 
 //Create a variable called lovesCode that is a boolean data type
 
-//Code here
+let lovesCode = true;
 
 //////////////////PROBLEM 4////////////////////
 
 //Create a variable called greatestFear that is undefined because we fear nothing
 
-//Code here
+let greatestFear = undefined;
+
 
 //////////////////PROBLEM 5////////////////////
 
